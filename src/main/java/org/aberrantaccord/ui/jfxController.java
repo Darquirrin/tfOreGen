371b66217fd34682285b04d9266090c5e8e623f5
Darquirrin <dos@nctv.com>
2015-03-16 21:41:27 -0500

    UI building

--- src/main/java/org/aberrantaccord/ui/jfxController.java
+++ src/main/java/org/aberrantaccord/ui/jfxController.java
@@ -1,4 +1,21 @@
 package org.aberrantaccord.ui;
 
+import javafx.event.ActionEvent;
+
 public class jfxController {
+    public void handleMenuAction_New(ActionEvent event) {
+        System.out.println("Info:" + event.toString() + "\n");
+    }
+    public void handleMenuAction_Open(ActionEvent event) {
+        System.out.println("Info:" + event.toString() + "\n");
+    }
+    public void handleMenuAction_Save(ActionEvent event) {
+        System.out.println("Info:" + event.toString() + "\n");
+    }
+    public void handleMenuAction_Close(ActionEvent event) {
+        System.out.println("Info:" + event.toString() + "\n");
+    }
+    public void handleMenuAction_Exit(ActionEvent event) {
+        System.out.println("Info:" + event.toString() + "\n");
+    }
 }
